9128c30cbc66772651495d102144f69b09f219c7
Bablakov <kbablakov@yandex.ru>
2023-12-17 16:46:14 +0300

    Commit1
    Реализовал анимации, движущейся за игроком камеры и движение заднего фона

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour // Отвечает за перемещение игрока
 {
@@ -17,8 +18,11 @@ public class PlayerController : MonoBehaviour // Отвечает за перемещение игрока
     public float checkRadius;        // На сколько близко игрок должен находиться к земле
     public LayerMask whatIsGround;   // Что мы считаем за земллю
 
+    private Animator anim;           // Отвечает за анимации
+
     private void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -26,18 +30,31 @@ public class PlayerController : MonoBehaviour // Отвечает за перемещение игрока
     {
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-        if (facingRight == false && moveInput > 0)
+       
+        if (facingRight == false && moveInput > 0)   // Блок кода отвечающий за поворот игрока
             Flip();
         else if (facingRight == true && moveInput < 0)
             Flip();
+
+        if (moveInput == 0)    // Блок кода отвечающий за анимации покоя и бега
+            anim.SetBool("IsRunning", false);
+        else
+            anim.SetBool("IsRunning", true);
     }
 
     private void Update()        // Прыжок игрока
     {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) // Блок кода отвечает за прыжок
+        {
             rb.velocity = Vector2.up * jumpForce;
+            anim.SetTrigger("TakeOf");
+        }
+        if (isGrounded == true)             // Блок кода отвечает за анимацию прыжка
+            anim.SetBool("IsJumping", false);
+        else
+            anim.SetBool("IsJumping", true);
     }
 
     void Flip()     // Поворот игрока
@@ -46,5 +63,17 @@ public class PlayerController : MonoBehaviour // Отвечает за перемещение игрока
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
+
+        if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.position = transform.position + new Vector3(0.65f, 0, 0);
+        }
+            
+        else if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.position = transform.position - new Vector3(0.65f, 0, 0);
+        }
     }
 }
